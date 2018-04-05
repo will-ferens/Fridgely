713f5ae6d51c15d79f7542fa20f8c8eb611c71d5
_dream <will.ferens@gmail.com>
2018-04-05 17:54:03 -0600

    initial commit

--- components/UserCamera.js
+++ components/UserCamera.js
@@ -1,91 +1,192 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Vibration, Image } from 'react-native'
-import { Camera, Permissions, FileSystem, Constants, AppLoading, Asset, CameraRoll } from 'expo'
+import React from 'react';
+import {
+  ActivityIndicator,
+  Button,
+  Clipboard,
+  Image,
+  Share,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Exponent, { Constants, ImagePicker, registerRootComponent } from 'expo';
 
-export default class UserCamera extends React.Component {
+export default class App extends React.Component {
   state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-    photos: [],
-    photoId: 1,
-    image: "",
-    ingredients: []
-  }
-
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA)
-    this.setState({ hasCameraPermission: status === 'granted' })
-  }
-
-  sendPhotoToServer = () => {
-    let image = this.state.image
-    fetch("https://pure-meadow-62546.herokuapp.com/image", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({image})
-    })
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-      // this.setState({
-      //   ingredients: data
-      // })
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }
-
-  takePicture = async function() {
-    const result = await Expo.ImagePicker.launchCameraAsync({
-      allowEditing: false,
-      exif: true
-    })
-    if(!result.cancelled) {
-      this.setState({
-        image: result.uri,
-        photoId: this.state.photoId + 1
-      })
-      Vibration.vibrate()
-      console.log(this.state.image)
-    }
-    CameraRoll.saveToCameraRoll(this.state.image)
-  }
+    image: null,
+    uploading: false,
+  };
 
   render() {
-    const { hasCameraPermission } = this.state
-    if (hasCameraPermission === null) {
-      return <View />
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>
-    } else {
-      return (
-        <View style={styles.container}>
-          <TouchableOpacity
-            onPress={this.takePicture.bind(this)}>
-            <Image style={{height: 50, width: 50}} source={require("../camera-logo.png")}/>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={this.sendPhotoToServer}>
-            <Text>Post</Text>
-          </TouchableOpacity>
-        </View>
-      )
-    }
+    let { image } = this.state;
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text
+          style={{
+            fontSize: 20,
+            marginBottom: 20,
+            textAlign: 'center',
+            marginHorizontal: 15,
+          }}>
+          Example: Upload ImagePicker result
+        </Text>
+
+        <Button
+          onPress={this._pickImage}
+          title="Pick an image from camera roll"
+        />
+
+        <Button onPress={this._takePhoto} title="Take a photo" />
+
+        {this._maybeRenderImage()}
+        {this._maybeRenderUploadingOverlay()}
+
+        <StatusBar barStyle="default" />
+      </View>
+    );
   }
+
+  _maybeRenderUploadingOverlay = () => {
+    if (this.state.uploading) {
+      return (
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: 'rgba(0,0,0,0.4)',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+          ]}>
+          <ActivityIndicator color="#fff" animating size="large" />
+        </View>
+      );
+    }
+  };
+
+  _maybeRenderImage = () => {
+    let { image } = this.state;
+    if (!image) {
+      return;
+    }
+
+    return (
+      <View
+        style={{
+          marginTop: 30,
+          width: 250,
+          borderRadius: 3,
+          elevation: 2,
+          shadowColor: 'rgba(0,0,0,1)',
+          shadowOpacity: 0.2,
+          shadowOffset: { width: 4, height: 4 },
+          shadowRadius: 5,
+        }}>
+        <View
+          style={{
+            borderTopRightRadius: 3,
+            borderTopLeftRadius: 3,
+            overflow: 'hidden',
+          }}>
+          <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+        </View>
+
+        <Text
+          onPress={this._copyToClipboard}
+          onLongPress={this._share}
+          style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
+          {image}
+        </Text>
+      </View>
+    );
+  };
+
+  _share = () => {
+    Share.share({
+      message: this.state.image,
+      title: 'Check out this photo',
+      url: this.state.image,
+    });
+  };
+
+  _copyToClipboard = () => {
+    Clipboard.setString(this.state.image);
+    alert('Copied image URL to clipboard');
+  };
+
+  _takePhoto = async () => {
+    let pickerResult = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _pickImage = async () => {
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _handleImagePicked = async pickerResult => {
+    let uploadResponse, uploadResult;
+
+    try {
+      this.setState({ uploading: true });
+
+      if (!pickerResult.cancelled) {
+        uploadResponse = await uploadImageAsync(pickerResult.uri);
+        uploadResult = await uploadResponse.json();
+        this.setState({ image: uploadResult.location });
+      }
+    } catch (e) {
+      console.log({ uploadResponse });
+      console.log({ uploadResult });
+      console.log({ e });
+      alert('Upload failed, sorry :(');
+    } finally {
+      this.setState({ uploading: false });
+    }
+  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingBottom: 30
-  },
-});
+async function uploadImageAsync(uri) {
+  let apiUrl = 'https://file-upload-example-backend-dkhqoilqqn.now.sh/upload';
+
+  // Note:
+  // Uncomment this if you want to experiment with local server
+  //
+  // if (Constants.isDevice) {
+  //   apiUrl = `https://your-ngrok-subdomain.ngrok.io/upload`;
+  // } else {
+  //   apiUrl = `http://localhost:3000/upload`
+  // }
+
+  let uriParts = uri.split('.');
+  let fileType = uriParts[uriParts.length - 1];
+
+  let formData = new FormData();
+  formData.append('photo', {
+    uri,
+    name: `photo.${fileType}`,
+    type: `image/${fileType}`,
+  });
+
+  let options = {
+    method: 'POST',
+    body: formData,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'multipart/form-data',
+    },
+  };
+
+  return fetch(apiUrl, options);
+}
